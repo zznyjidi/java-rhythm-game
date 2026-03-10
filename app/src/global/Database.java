dde89d3ce60eaca11bf110feada2b7b197e4c832
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2026-03-10 13:11:35 -0400

    add: keybind

--- app/src/global/Database.java
+++ app/src/global/Database.java
@@ -14,5 +14,11 @@ public class Database {
     public static final int JUDGEMENT_LATE_RANGE = JUDGEMENT_GREAT_RANGE;
     public static final int JUDGEMENT_EARLY_RANGE = JUDGEMENT_LATE_RANGE + JUDGEMENT_EARLY_MISS_WINDOW;
 
+    public static char[][] TRACK_KEYBIND = {
+            { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' },
+            { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';' },
+            { 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/' }
+    };
+
     public static Chart currentChart;
 }

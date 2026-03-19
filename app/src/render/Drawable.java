172782da3a4f88966a17a8c13b18dda1157a2154
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2026-03-19 18:37:21 -0400

    change: pass frameTime to isExpired

--- app/src/render/Drawable.java
+++ app/src/render/Drawable.java
@@ -6,7 +6,7 @@ import java.awt.Graphics2D;
 public interface Drawable {
     public void drawElement(Graphics2D graphics2d, Dimension screenSize, long frameTime);
 
-    public boolean isExpired();
+    public boolean isExpired(long frameTime);
 
     @Override
     int hashCode();
